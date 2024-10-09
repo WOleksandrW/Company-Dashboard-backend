be73bed8ab2a42633073f0cd27b031a5e67abbe7
WOleksandrW <oleks.221.39@gmail.com>
2024-10-09 07:25:12 +0000

    feat: add checkIsExist method and exceptions to CompaniesService

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { Company } from './entities/company.entity';
@@ -17,27 +17,53 @@ export class CompaniesService {
   async create(createCompanyDto: CreateCompanyDto) {
     const { userId, ...rest } = createCompanyDto;
 
+    if (await this.checkIsExist({ title: rest.title })) {
+      throw new ConflictException('Title is already in use.');
+    }
+
     const user = await this.usersService.findOne(userId);
 
     return this.companiesRepository.insert({ ...rest, user });
   }
 
-  findAll() {
-    return this.companiesRepository.find({
+  async findAll() {
+    const list = await this.companiesRepository.find({
       relations: { user: true }
     });
+
+    if (list.length === 0) {
+      throw new NotFoundException('Companies List Not Found');
+    }
+
+    return list;
   }
 
-  findOne(id: number) {
-    return this.companiesRepository.findOne({
+  async findOne(id: number) {
+    const company = await this.companiesRepository.findOne({
       where: { id },
       relations: { user: true }
     });
+
+    if (!company) {
+      throw new NotFoundException('Company Not Found');
+    }
+
+    return company;
+  }
+
+  checkIsExist(where: FindOptionsWhere<Company>) {
+    return this.companiesRepository.exists({ where, withDeleted: true });
   }
 
   async update(id: number, updateCompanyDto: UpdateCompanyDto) {
+    const company = await this.findOne(id);
+
     const { userId, ...rest } = updateCompanyDto;
     let body: { user?: User } = {};
+
+    if (rest.title && company.title !== rest.title && await this.checkIsExist({ title: rest.title })) {
+      throw new ConflictException('Title is already in use.');
+    }
 
     if (userId) {
       body.user = await this.usersService.findOne(userId);
@@ -46,7 +72,9 @@ export class CompaniesService {
     return this.companiesRepository.update(id, { ...rest, ...body });
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    await this.findOne(id);
+
     return this.companiesRepository.softDelete({ id });
   }
 }
