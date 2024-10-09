6890f3f6b6cfb92024436fa1fff10aae38524442
WOleksandrW <oleks.221.39@gmail.com>
2024-10-09 07:19:49 +0000

    feat: add checkIsExist method and exceptions to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,26 +17,52 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const { password, ...rest } = createUserDto;
 
+    if (await this.checkIsExist({ email: rest.email })) {
+      throw new ConflictException('Email is already in use.');
+    }
+
     const hashPassword = await bcrypt.hash(password, this.saltRounds);
 
     return this.usersRepository.insert({ ...rest, password: hashPassword });
   }
 
-  findAll() {
-    return this.usersRepository.find();
+  async findAll() {
+    const list = await this.usersRepository.find();
+
+    if (list.length === 0) {
+      throw new NotFoundException('Users List Not Found');
+    }
+
+    return list;
   }
 
-  findOne(id: number) {
-    return this.usersRepository.findOneBy({ id });
+  async findOne(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+
+    return user;
   }
 
   findOneByEmail(email: string) {
     return this.usersRepository.findOneBy({ email });
   }
 
+  checkIsExist(where: FindOptionsWhere<User>) {
+    return this.usersRepository.exists({ where, withDeleted: true });
+  }
+
   async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+
     const { password, ...rest } = updateUserDto;
     let body: { password?: string } = {};
+
+    if (rest.email && user.email !== rest.email && await this.checkIsExist({ email: rest.email })) {
+      throw new ConflictException('Email is already in use.');
+    }
 
     if (password) {
       body.password = await bcrypt.hash(password, this.saltRounds);
@@ -45,7 +71,9 @@ export class UsersService {
     return this.usersRepository.update(id, { ...rest, ...body });
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    await this.findOne(id);
+
     return this.usersRepository.softDelete({ id });
   }
 }
